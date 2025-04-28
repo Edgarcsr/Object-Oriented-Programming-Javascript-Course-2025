1d1e6ac04fc993b7e15223f2a91a682d4261dfd6
Edgarcsr <edgarseabra@outlook.com.br>
2025-04-28 17:10:04 -0300

    📙 - Construtores

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {};
+const Person = function (firstName, birthYear) {
+  // Instace properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 
-new Person('Jonas', 1991);
+  // Never do this, it will create multiples functions with the same inner code
+  //   this.calcAge = function(){
+  //     console.log(2037 - this.birthYear);
+  //   }
+};
+
+const jonas = new Person('Jonas', 1991);
+const matilda = new Person('Matilda', 2006);
+const edgar = new Person('Edgar', 2004);
