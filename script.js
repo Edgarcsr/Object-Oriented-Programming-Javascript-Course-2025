fe4efffddb8d7a3e452c1b836f74092338c60d9a
Edgarcsr <edgarseabra@outlook.com.br>
2025-04-29 12:10:29 -0300

    📙 - Outra maneira de utilizar Classes em JS

--- script.js
+++ script.js
@@ -1,40 +1,95 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  // Instace properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   // Instace properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // Never do this, it will create multiples functions with the same inner code
-  //   this.calcAge = function(){
-  //     console.log(2037 - this.birthYear);
-  //   }
-};
+//   // Never do this, it will create multiples functions with the same inner code
+//   //   this.calcAge = function(){
+//   //     console.log(2037 - this.birthYear);
+//   //   }
+// };
 
-const jonas = new Person('Jonas', 1991);
-const matilda = new Person('Matilda', 2006);
-const edgar = new Person('Edgar', 2004);
+// const jonas = new Person('Jonas', 1991);
+// const matilda = new Person('Matilda', 2006);
+// const edgar = new Person('Edgar', 2004);
 
-// Prototypes
-Person.prototype.calcAge = function () {
-  console.log(2025 - this.birthYear);
-};
+// // Prototypes
+// Person.prototype.calcAge = function () {
+//   console.log(2025 - this.birthYear);
+// };
 
-jonas.calcAge();
-edgar.calcAge();
+// jonas.calcAge();
+// edgar.calcAge();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(edgar));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(edgar));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
-// .prototypeOfLinkedObjects
+// // .prototypeOfLinkedObjects
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas, edgar);
-console.log(jonas.species, edgar.species);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas, edgar);
+// console.log(jonas.species, edgar.species);
 
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
+
+// console.log(edgar.__proto__);
+// // Object.prototype (top of prototype chain)
+// console.log(edgar.__proto__.__proto__);
+// console.log(edgar.__proto__.__proto__.__proto__);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7]; // new array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+
+// Class expression
+// const PersonCL = class {}
+
+// Class declaration
+
+class PersonCL {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}!`);
+  }
+}
+
+const jessica = new PersonCL('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCL.prototype);
+
+// PersonCL.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}!`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
